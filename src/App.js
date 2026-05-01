e30ec56db02d1f2a468b3b0d9a00cb890274e28c
anastasia1ten <neymananastasia73@gmail.com>
2026-05-01 23:09:37 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from'../src/components/head/index'
+import Main from'../src/components/main/index'
+import Footer from'../src/components/footer/index'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
